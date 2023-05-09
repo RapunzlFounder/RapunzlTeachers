51baeb6ccacf3374fe1ef72c08afdec0d0889b16
Brian Curcio <bcurcio@rapunzlinvestments.com>
2023-05-08 21:11:05 -0400

    Improved PDF Formatting

--- src/components/Admin/PDFViewer.js
+++ src/components/Admin/PDFViewer.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Dialog from '@mui/material/Dialog';
 import { Document, Page, pdfjs } from "react-pdf";
 import '../../styles/Admin/PDF.css';
@@ -10,25 +10,37 @@ import CircularProgress from '@mui/material/CircularProgress';
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
 const PDFViewer = (props) => {
+  // Allows Us To Determine Current Page Number, Total Number Of Pages & Update These State Values
   const [numPages, setNumPages] = useState(null);
   const [pageNumber, setPageNumber] = useState(1);
 
+  // Function Component Equivalent Of ComponentDidUpdate Which Resets The State When Visibility Is Toggled
+  // This Sets Page Back To 1 To Avoid Going Out Of Bounds On Different Resources.
+  useEffect(() => {
+    setNumPages(null);
+    setPageNumber(1);
+  }, [props.visible]);
+
+  // Updates The Number Of Pages When The PDF Successfully Loads
   function onDocumentLoadSuccess({ numPages }) {
     setNumPages(numPages);
   }
 
+  // Handles Incrementing The Page Number By One. Will Not Exceed The Total Number Of Pages
   function _nextPDFPage() {
     if (pageNumber + 1 <= numPages) {
       setPageNumber(pageNumber + 1);
     }
   }
 
+  // Handles Decrementing The Page Number By One. Will Not Decrease Below 1 For The First Page Of The PDF.
   function _previousPDFPage() {
     if (pageNumber - 1 > 0) {
       setPageNumber(pageNumber - 1)
     }
   }
 
+  // Returns Component If There Is An Error Loading The PDF
   function errorPDFComponent() {
     return (
       <div>
@@ -37,6 +49,7 @@ const PDFViewer = (props) => {
     )
   }
   
+  // Returns A Loading Component While We Retrieve The PDF Or Render Each Page For The First Time
   function loadingPDFComponent() {
     return (
       <div className='pdf-loading-page-container'>
